4f70b96d8a39226fd68204658a252631d563aead
MyFriend <myfriend.jmc@gmail.com>
2025-10-25 21:37:27 -0500

    metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -34,3 +34,8 @@ def get_starwars_warriors():
 def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
+
+@app.get("/metodos de platzi")
+def get_cursos():
+    rows = ["Docker", "Bash", "Linux", "Inglés", "Python", "Javascript", "Git", "GitHub"]
+    return rows
